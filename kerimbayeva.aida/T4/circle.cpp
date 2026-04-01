4ffbfbe15bfa421cb9f927cc585abeaf81c76749
aida kerimbayeva <kerimbayeva06@mail.ru>
2026-04-01 17:38:54 +0300

    Add empty lines at end of files for GitHub

---
+++ kerimbayeva.aida/T4/circle.cpp
@@ -0,0 +1,36 @@
+#include "circle.hpp"
+#include <stdexcept>
+#include <cmath>
+#include <numbers>
+
+
+Circle::Circle(const Point& center, double radius)
+    : center_(center), radius_(radius) {
+    if (radius <= 0) {
+        throw std::invalid_argument("Radius must be positive");
+    }
+}
+
+double Circle::getArea() const {
+    return std::numbers::pi * radius_ * radius_;
+}
+
+Point Circle::getCenter() const {
+    return center_;
+}
+
+void Circle::move(double dx, double dy) {
+    center_.x += dx;
+    center_.y += dy;
+}
+
+void Circle::scale(double factor) {
+    if (factor <= 0) {
+        throw std::invalid_argument("Scale factor must be positive");
+    }
+    radius_ *= factor;
+}
+
+std::string Circle::getName() const {
+    return "CIRCLE";
+}
